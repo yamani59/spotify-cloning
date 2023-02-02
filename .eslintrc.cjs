359ee33d82eb9f91f889d08245fb87fc87012346
yamani59 <yamaniyud@gmail.com>
2023-02-02 17:17:16 +0700

    chore: setup husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/jsx-no-target-blank': 'off',
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
