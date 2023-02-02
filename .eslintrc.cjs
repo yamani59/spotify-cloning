59b8cb0a9c5f09b9602310056fe7ad793888efdc
yamani59 <yamaniyud@gmail.com>
2023-02-02 17:21:11 +0700

    chore: try error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/jsx-no-target-blank': 'off',
     '@typescript-eslint/no-confusing-void-expression': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
   settings: {
     react: {
